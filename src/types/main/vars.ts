c20a7ed6d5808aa4fbba160b61982e7b37b4db9d
S.Watanabe <ocelot2828@gmail.com>
2025-03-20 18:32:15 +0900

    feat: rename CSS variable Properties to Property

--- src/types/main/vars.ts
+++ src/types/main/vars.ts
@@ -3,5 +3,5 @@ export type VarsDefinition = Record<string, string | number | Record<string, str
 type CSSVariableKey = `--${string}`;
 export type CSSVariableValue = `var(${CSSVariableKey})`;
 
-type CSSVariableProperties = { [key: CSSVariableKey]: string | number };
-export type VarsTransformed = { [key: string]: CSSVariableProperties };
+export type CSSVariableProperty = { [key: CSSVariableKey]: string | number };
+export type VarsTransformed = { [key: string]: CSSVariableProperty };
