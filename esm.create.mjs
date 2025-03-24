7dfa123ca9ee9c895984d37e6f55ac618a53a119
S.Watanabe <ocelot2828@gmail.com>
2025-03-24 14:43:34 +0900

    feat: script removed the server directive from ESM build.mjs

--- esm.create.mjs
+++ esm.create.mjs
@@ -10,4 +10,20 @@ import { globSync } from 'fs';
       await fs.rename(file, newPath);
     })
   );
+  const buildFile = './dist/utils/build.mjs';
+  try {
+    let content = await fs.readFile(buildFile, 'utf-8');
+    const lines = content.split('\n');
+
+    // A first line delete
+    lines.shift();
+    const updatedContent = lines.join('\n');
+    await fs.writeFile(buildFile, updatedContent);
+
+    console.log('Removed the first line in build.mjs');
+  } catch (error) {
+    console.error('Error processing build.mjs:', error);
+  } finally {
+    console.log('ESM build process completed successfully');
+  }
 })();
