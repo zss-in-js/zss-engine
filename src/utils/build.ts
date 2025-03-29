7ff5bdf05bd9d16e8cc96ad7cfc39c58fcc1dcc7
S.Watanabe <ocelot2828@gmail.com>
2025-03-29 21:43:56 +0900

    fix: use static import

--- src/utils/build.ts
+++ src/utils/build.ts
@@ -1,20 +1,16 @@
 'use server';
+import fs from 'fs';
+import { styleText } from 'util';
 import { isServer } from './helper.js';
 
 export const build = async (styleSheet: string, filePath: string, global?: string) => {
   if (!isServer) return;
 
-  const fs = require('fs');
-  const { styleText } = require('util');
-
   const message = global === '--global' ? styleText('underline', `✅Generated global CSS\n\n`) : styleText('underline', `✅Generated create CSS\n\n`);
   try {
     if (fs.existsSync(filePath)) {
-      const cssData = fs.readFileSync(filePath, 'utf-8');
-      if (!cssData.includes(styleSheet)) {
-        fs.appendFileSync(filePath, styleSheet, 'utf-8');
-        if (process.argv.includes('--log')) console.log(message + styleSheet);
-      }
+      fs.appendFileSync(filePath, styleSheet, 'utf-8');
+      if (process.argv.includes('--log')) console.log(message + styleSheet);
     }
     return;
   } catch (error) {
