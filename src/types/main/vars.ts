18980f4f152b8a9b449277772b56ae74b50b92e4
refirst11 <ocelot2828@gmail.com>
2025-06-03 17:26:42 +0900

    feat: CreateVars to CreateValues

--- src/types/main/vars.ts
+++ src/types/main/vars.ts
@@ -1,5 +1,6 @@
 type CSSVariableKey = `--${string}`;
 export type CSSVariableValue = `var(${CSSVariableKey})`;
 export type CSSVariableProperty = { [key: CSSVariableKey]: string | number };
-export type CreateVars = Record<string, string | number>;
+
+export type CreateValues = Record<string, string | number>;
 export type CreateTheme = Record<string, Record<string, string | number>>;
