91f905e241731c0910acaf4e533ed39e08ec093c
refirst11 <ocelot2828@gmail.com>
2025-07-05 14:17:32 +0900

    feat(types/create.ts): add hover AtomicClass property and value for Type system

--- src/types/main/create.ts
+++ src/types/main/create.ts
@@ -12,9 +12,15 @@ type Selector<Properties> = {
   readonly properties: Properties;
 };
 
+type AtomicClass<P extends string, V> = {
+  readonly property: `${P}: ${V & (string | number)}`;
+};
+
 export type ReturnType<T> = {
   [K in keyof T]: Readonly<{
-    [P in keyof T[K]]: P extends `@media ${string}` | `@container ${string}` | `:${string}` | `&${string}` ? Selector<keyof T[K][P]> : Readonly<T[K][P]>;
+    [P in keyof T[K]]: P extends `@media ${string}` | `@container ${string}` | `:${string}` | `&${string}`
+      ? Selector<keyof T[K][P]>
+      : AtomicClass<P & string, T[K][P]>;
   }>;
 } & {
   [K in keyof T as `$${string & K}`]: string;
