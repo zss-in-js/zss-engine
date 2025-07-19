64aa19bd1adba52be0c246ef1f3fd771c25611f3
refirst11 <ocelot2828@gmail.com>
2025-07-19 09:20:21 +0900

    feat(processor-atomic.ts): fix shorthand vs longhand properties

--- src/utils/processor-atomic.ts
+++ src/utils/processor-atomic.ts
@@ -1,5 +1,6 @@
 import type { CSSProperties, CreateStyle } from '../index.js';
 import { camelToKebabCase, genBase36Hash, applyCssValue, transpileAtomic } from '../index.js';
+import { SHORTHAND_PROPERTIES } from './shorthand';
 
 function splitAtomicAndNested(obj: CSSProperties, flat: CreateStyle, nonFlat: CreateStyle) {
   Object.entries(obj).forEach(([property, value]) => {
@@ -19,27 +20,59 @@ function splitAtomicAndNested(obj: CSSProperties, flat: CreateStyle, nonFlat: Cr
   });
 }
 
+const LONG_TO_SHORT: Record<string, string> = {};
+Object.entries(SHORTHAND_PROPERTIES).forEach(([shorthand, longhands]) => {
+  longhands.forEach(long => {
+    LONG_TO_SHORT[long] = shorthand;
+  });
+});
+
 function processAtomicProps(flatProps: CreateStyle, atomicHashes: Set<string>, allStyleSheets: Set<string>, parentAtRule?: string) {
-  Object.entries(flatProps).forEach(([property, value]) => {
+  const seen = new Set<string>();
+  const resultQueue: Array<[string, string | number]> = [];
+
+  for (const [property, value] of Object.entries(flatProps)) {
     if (property.startsWith('@media') || property.startsWith('@container')) {
       processAtomicProps(value as CreateStyle, atomicHashes, allStyleSheets, property);
-    } else {
-      const CSSProp = camelToKebabCase(property);
-      const normalizedValue = applyCssValue(value as string | number, CSSProp);
-      const singlePropObj = { [property]: normalizedValue };
-      const styleObj = parentAtRule ? { [parentAtRule]: singlePropObj } : singlePropObj;
-      const atomicHash = genBase36Hash(styleObj, 1, 8);
-
-      if (atomicHashes.has(atomicHash)) return;
-      atomicHashes.add(atomicHash);
-
-      let styleSheet = transpileAtomic(property, value as string | number, atomicHash);
-      if (parentAtRule) {
-        styleSheet = `${parentAtRule} { ${styleSheet} }`;
-      }
-      allStyleSheets.add(styleSheet + '\n');
+      continue;
     }
-  });
+
+    const kebab = camelToKebabCase(property);
+
+    if (SHORTHAND_PROPERTIES[kebab]) {
+      const longhands = SHORTHAND_PROPERTIES[kebab];
+      longhands.forEach(l => seen.delete(l));
+      seen.add(kebab);
+      resultQueue.push([property, value as string | number]);
+    } else if (kebab in LONG_TO_SHORT) {
+      const shorthand = LONG_TO_SHORT[kebab];
+      if (seen.has(shorthand)) {
+        continue;
+      }
+      seen.add(kebab);
+      resultQueue.push([property, value as string | number]);
+    } else {
+      seen.add(kebab);
+      resultQueue.push([property, value as string | number]);
+    }
+  }
+
+  for (const [property, value] of resultQueue) {
+    const CSSProp = camelToKebabCase(property);
+    const normalizedValue = applyCssValue(value, CSSProp);
+    const singlePropObj = { [property]: normalizedValue };
+    const styleObj = parentAtRule ? { [parentAtRule]: singlePropObj } : singlePropObj;
+    const atomicHash = genBase36Hash(styleObj, 1, 8);
+
+    if (atomicHashes.has(atomicHash)) continue;
+    atomicHashes.add(atomicHash);
+
+    let styleSheet = transpileAtomic(property, value, atomicHash);
+    if (parentAtRule) {
+      styleSheet = `${parentAtRule} { ${styleSheet} }`;
+    }
+    allStyleSheets.add(styleSheet + '\n');
+  }
 }
 
 export { splitAtomicAndNested, processAtomicProps };
