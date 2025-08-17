73fec15c8d8b5f53543c6fabca2e3a4fa1a4982f
refirst11 <ocelot2828@gmail.com>
2025-08-17 21:49:27 +0900

    feat: add isHashInStyleSheets and remove hasWebpackPlugin

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,4 @@ export { build } from './utils/build.js';
 export { camelToKebabCase, applyCssValue } from './utils/helper.js';
 export { injectClientCSS } from './utils/inject-client-css.js';
 export { injectClientGlobalCSS } from './utils/inject-client-global-css.js';
-export { hasWebpackPlugin } from './utils/has-webpack-plugin';
+export { isHashInStyleSheets } from './utils/is-hash-in-style-sheets.js';
