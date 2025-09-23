603a062fbb8eecf12f002ecf39b43216a0cf98a9
refirst11 <ocelot2828@gmail.com>
2025-09-23 10:07:05 +0900

    feat(types): fix CreateTheme changed to CreateTokens

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export type { CSSProperties } from './types/common/css-properties';
 export type { CreateStyleType, ReturnType, CreateStyle } from './types/main/create';
 export type { CSSHTML, CreateKeyframes } from './types/main/global';
-export type { CreateValues, CreateTheme, ReturnVariableType, RxVariableSet, ReturnRx } from './types/main/vars';
+export type { CreateValues, CreateTokens, ReturnVariableType, RxVariableSet, ReturnRx } from './types/main/vars';
 export { isServer, isDevelopment, isTestingDevelopment } from './utils/helper.js';
 export { genBase36Hash } from './utils/hash.js';
 export { transpile } from './utils/transpile.js';
--- src/types/main/vars.ts
+++ src/types/main/vars.ts
@@ -3,7 +3,7 @@ export type CSSVariableValue = `var(${CSSVariableKey})`;
 export type CSSVariableProperty = { [key: CSSVariableKey]: string | number };
 
 export type CreateValues = Record<string, string | number>;
-export type CreateTheme = Record<string, Record<string, string | number>>;
+export type CreateTokens = Record<string, Record<string, string | number>>;
 export type ReturnVariableType<T> = { [K in keyof T]: CSSVariableValue };
 
 export type RxVariableSet = { [key: CSSVariableKey]: string };
