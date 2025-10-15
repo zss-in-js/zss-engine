0f6fda709e008df1d4a89df46edbaf9b18e972eb
refirst11 <ocelot2828@gmail.com>
2025-10-15 18:38:57 +0900

    feat(transpile-atomic.ts): added logic to increase longhand specificity

--- src/utils/transpile-atomic.ts
+++ src/utils/transpile-atomic.ts
@@ -1,10 +1,19 @@
 import { applyCssValue, camelToKebabCase } from './helper.js';
+import { SHORTHAND_PROPERTIES } from './shorthand.js';
+
+const ALL_LONGHANDS = new Set(Object.values(SHORTHAND_PROPERTIES).flat());
 
 function transpileAtomic(property: string, value: string | number, hash: string): string {
   if (typeof value === 'string' || typeof value === 'number') {
     const CSSProp = camelToKebabCase(property);
     const applyValue = applyCssValue(value, CSSProp);
-    const atomicRule = `.${hash} { ${CSSProp}: ${applyValue}; }`;
+
+    let selector = `.${hash}`;
+    if (ALL_LONGHANDS.has(CSSProp)) {
+      selector += ':not(#\\#)';
+    }
+
+    const atomicRule = `${selector} { ${CSSProp}: ${applyValue}; }`;
 
     if (property.startsWith('@media') || property.startsWith('@container')) {
       return `${property} { ${atomicRule} }`;
