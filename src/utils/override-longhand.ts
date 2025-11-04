17fb0461c5bde4b67370dcb837c61987b48c653d
refirst11 <ocelot2828@gmail.com>
2025-11-04 11:25:54 +0900

    feat(override-longhand.ts): added

---
+++ src/utils/override-longhand.ts
@@ -0,0 +1,52 @@
+import { camelToKebabCase, CreateStyle, LONG_TO_SHORT, SHORTHAND_PROPERTIES } from '..';
+
+export const overrideLonghand = (style: CreateStyle) => {
+  const props = Object.keys(style);
+  const propsToRemove = new Set<string>();
+
+  // Find plain props and their indices
+  const plainProps: { key: string; index: number }[] = [];
+  for (let i = 0; i < props.length; i++) {
+    if (!props[i].startsWith('@')) {
+      plainProps.push({ key: props[i], index: i });
+    }
+  }
+
+  // Find shorthands among plain props
+  const shorthandsInStyle: { [key: string]: number } = {};
+  for (const { key, index } of plainProps) {
+    const kebab = camelToKebabCase(key);
+    if (SHORTHAND_PROPERTIES[kebab]) {
+      shorthandsInStyle[kebab] = index;
+    }
+  }
+
+  // Determine which longhands to remove
+  for (const { key, index } of plainProps) {
+    const kebab = camelToKebabCase(key);
+    if (!SHORTHAND_PROPERTIES[kebab]) {
+      // is longhand
+      const longhands = LONG_TO_SHORT[kebab] || [];
+      for (const shorthand of longhands) {
+        if (shorthandsInStyle[shorthand] > index) {
+          propsToRemove.add(key);
+          break;
+        }
+      }
+    }
+  }
+
+  const finalStyle: CreateStyle = {};
+  for (const prop of props) {
+    if (propsToRemove.has(prop)) {
+      continue;
+    }
+    if (prop.startsWith('@')) {
+      finalStyle[prop] = overrideLonghand(style[prop] as CreateStyle);
+    } else {
+      finalStyle[prop] = style[prop];
+    }
+  }
+
+  return finalStyle;
+};
