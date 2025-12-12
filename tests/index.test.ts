10871d17877755a2a64a7bc2201a46c6583ca3d8
refirst11 <ocelot2828@gmail.com>
2025-12-12 16:23:19 +0900

    fix: tests removed unused function

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -8,11 +8,9 @@ describe('index exports', () => {
     expect(index.genBase36Hash).toBeDefined();
     expect(index.transpile).toBeDefined();
     expect(index.transpileAtomic).toBeDefined();
-    expect(index.transformNestedSelectors).toBeDefined();
     expect(index.splitAtomicAndNested).toBeDefined();
     expect(index.processAtomicProps).toBeDefined();
     expect(index.overrideLonghand).toBeDefined();
-
     expect(index.build).toBeDefined();
     expect(index.camelToKebabCase).toBeDefined();
     expect(index.applyCssValue).toBeDefined();
@@ -22,7 +20,6 @@ describe('index exports', () => {
     expect(typeof index.genBase36Hash).toBe('function');
     expect(typeof index.transpile).toBe('function');
     expect(typeof index.transpileAtomic).toBe('function');
-    expect(typeof index.transformNestedSelectors).toBe('function');
     expect(typeof index.splitAtomicAndNested).toBe('function');
     expect(typeof index.processAtomicProps).toBe('function');
     expect(typeof index.overrideLonghand).toBe('function');
