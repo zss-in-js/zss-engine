d03eecd0bbe906d5d44c7ce8d0ec8359f1aa3e5a
refirst11 <ocelot2828@gmail.com>
2025-12-12 12:30:24 +0900

    chore(variableTypes.ts): update

--- src/types/main/variableTypes.ts
+++ src/types/main/variableTypes.ts
@@ -4,11 +4,11 @@ export type CSSVariableProperty = { [key: CSSVariableKey]: string | number };
 
 export type CreateValues = Record<string, string | number>;
 
-export type CreateTokens = Record<string, Record<string, string | number>>;
+export type CreateTheme = Record<string, Record<string, string | number>>;
 export type ReturnVariableType<T> = { [K in keyof T]: CSSVariableValue };
 
-export type RxVariableSet = { [key: CSSVariableKey]: string };
-export type ReturnRx = {
+export type XVariableSet = { [key: CSSVariableKey]: string };
+export type ReturnX = {
   className: string;
   style: {
     [k: string]: string;
