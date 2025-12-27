dafc9e9d28c4e00b3096e85c6c9e069aa6fb84ed
refirst11 <ocelot2828@gmail.com>
2025-12-27 21:24:07 +0900

    feat(types): update CreateStatic

--- src/types/main/variableTypes.ts
+++ src/types/main/variableTypes.ts
@@ -2,15 +2,9 @@ type CSSVariableKey = `--${string}`;
 export type CSSVariableValue = `var(${CSSVariableKey})`;
 export type CSSVariableProperty = { [key: CSSVariableKey]: string | number };
 
-export type CreateValues = Record<string, string | number>;
+export type CreateStatic = Record<string, string | number>;
 
 export type CreateTheme = Record<string, Record<string, string | number>>;
 export type ReturnVariableType<T> = { [K in keyof T]: CSSVariableValue };
 
-export type XVariableSet = { [key: CSSVariableKey]: string };
-export type ReturnX = {
-  className: string;
-  style: {
-    [k: string]: string;
-  };
-};
+export type Styles = { [key: CSSVariableKey]: string };
