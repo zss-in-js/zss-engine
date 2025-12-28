2d62bdef0143f16f6b10e04f8f61fb0e58f4239e
refirst11 <ocelot2828@gmail.com>
2025-12-28 14:45:21 +0900

    fix: export paths update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export type { CSSProperties } from './types/common/css-properties';
 export type { CreateStyleType, ReturnType, CreateStyle } from './types/main/create';
 export type { CSSHTML, Keyframes } from './types/main/global';
 export type { CreateStatic, CreateTheme, ReturnVariableType, Styles } from './types/main/variableTypes';
-export type { ViewTransition } from './types/main/viewTransitionOptions';
+export type { ViewTransition } from './types/main/viewTransition';
 export { isServer, isDevelopment, isTestingDevelopment } from './utils/helper.js';
 export { genBase36Hash } from './utils/hash.js';
 export { transpile } from './utils/transpile.js';
