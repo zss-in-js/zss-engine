0468995de51886d253344f2f63b341eff4b7e2a2
refirst11 <ocelot2828@gmail.com>
2026-01-26 23:36:20 +0900

    fix(types/variableTypes.ts): rm unused type

--- src/types/main/variableTypes.ts
+++ src/types/main/variableTypes.ts
@@ -1,10 +1,4 @@
 type CSSVariableKey = `--${string}`;
 export type CSSVariableValue = `var(${CSSVariableKey})`;
 export type CSSVariableProperty = { [key: CSSVariableKey]: string | number };
-
-export type CreateStatic = Record<string, string | number>;
-
 export type CreateTheme = Record<string, Record<string, string | number>>;
-export type ReturnVariableType<T> = { [K in keyof T]: CSSVariableValue };
-
-export type Styles = { [key: CSSVariableKey]: string };
