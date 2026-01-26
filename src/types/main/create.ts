2327e46a434dd026a13a0e37f0b3528132df7245
refirst11 <ocelot2828@gmail.com>
2026-01-26 23:35:35 +0900

    fix(types/create.ts): rm unused type

--- src/types/main/create.ts
+++ src/types/main/create.ts
@@ -1,19 +1,5 @@
 import type { CSSProperties } from '../common/css-properties';
 
-export type CreateStyleType<T> = {
-  readonly [K in keyof T]: T[K] extends CSSProperties ? CSSProperties : T[K];
-};
-
 export type CreateStyle = {
   [key: string]: CSSProperties;
-};
-
-type Selector<Properties> = {
-  readonly properties: Properties;
-};
-
-export type ReturnType<T> = {
-  [K in keyof T]: Readonly<{
-    [P in keyof T[K]]: P extends `@media ${string}` | `@container ${string}` | `:${string}` | `&${string}` ? Selector<keyof T[K][P]> : T[K][P];
-  }>;
 };
