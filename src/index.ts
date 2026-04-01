4085bed06d2f760fc48f0e40bec00e4398f4da1c
refirst11 <ocelot2828@gmail.com>
2026-04-01 11:20:32 +0900

    feat: add export exceptionCamelCase

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,4 @@ export { transpile } from './utils/transpile.js';
 export { transpileAtomic } from './utils/transpile-atomic.js';
 export { splitAtomicAndNested, processAtomicProps } from './utils/processor-atomic.js';
 export { overrideLonghand } from './utils/override-longhand.js';
-export { camelToKebabCase, applyCssValue } from './utils/helper.js';
+export { camelToKebabCase, applyCssValue, exceptionCamelCase } from './utils/helper.js';
