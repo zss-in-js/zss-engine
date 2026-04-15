0a912de80625be62a4d9c80305a7ea7a3cab9013
refirst11 <ocelot2828@gmail.com>
2026-04-15 18:14:25 +0900

    chore(variableTypes.ts): remove

--- src/types/main/variableTypes.ts
+++
@@ -1,4 +0,0 @@
-type CSSVariableKey = `--${string}`;
-export type CSSVariableValue = `var(${CSSVariableKey})`;
-export type CSSVariableProperty = { [key: CSSVariableKey]: string | number };
-export type CreateTheme = Record<string, Record<string, string | number>>;
