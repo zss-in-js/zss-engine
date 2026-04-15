922ee5d9890098dc7f1a55fa41470ffcb5c572e7
refirst11 <ocelot2828@gmail.com>
2026-04-15 18:14:37 +0900

    chore(viewTransition.ts): remove

--- src/types/main/viewTransition.ts
+++
@@ -1,8 +0,0 @@
-import { CSSProperties } from '../common/css-properties';
-
-export type ViewTransition = {
-  group?: CSSProperties;
-  imagePair?: CSSProperties;
-  new?: CSSProperties;
-  old?: CSSProperties;
-};
