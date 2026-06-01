19f495508baf51e59b9d1c0d0514ef90051a5939
refirst11 <ocelot2828@gmail.com>
2026-06-01 19:17:13 +0900

    feat: add export isAtRule

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export { transpile } from './utils/transpile.js';
 export { transpileAtomic } from './utils/transpile-atomic.js';
 export { splitAtomicAndNested, processAtomicProps } from './utils/processor-atomic.js';
 export { overrideLonghand } from './utils/override-longhand.js';
-export { camelToKebabCase, applyCssValue, exceptionCamelCase } from './utils/helper.js';
+export { camelToKebabCase, applyCssValue, isAtRule, exceptionCamelCase } from './utils/helper.js';
