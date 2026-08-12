957957599c68cafdcd742a48fbebd3e8a11db248
refirst11 <ocelot2828@gmail.com>
2026-08-12 14:37:07 +0900

    chore: export MAX_PROPERTY_DEPPTH

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export { transpile } from './utils/transpile.js';
 export { transpileAtomic } from './utils/transpile-atomic.js';
 export { splitAtomicAndNested, processAtomicProps } from './utils/processor-atomic.js';
 export { overrideLonghand } from './utils/override-longhand.js';
+export { MAX_PROPERTY_DEPTH } from './utils/shorthand.js';
 export { camelToKebabCase, applyCssValue, isAtRule, exceptionCamelCase } from './utils/helper.js';
