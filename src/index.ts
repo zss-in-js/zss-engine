b7a5b2140d9cbd5c0bae4816535a636db8a41854
refirst11 <ocelot2828@gmail.com>
2026-08-12 18:11:47 +0900

    fix: remove export overrideLonghand in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ export { genBase36Hash } from './utils/hash.js';
 export { transpile } from './utils/transpile.js';
 export { transpileAtomic } from './utils/transpile-atomic.js';
 export { splitAtomicAndNested, processAtomicProps } from './utils/processor-atomic.js';
-export { overrideLonghand } from './utils/override-longhand.js';
 export { camelToKebabCase, applyCssValue, isAtRule, exceptionCamelCase } from './utils/helper.js';
