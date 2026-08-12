baec5dbfe8b4cbab5d14e9e41c7b252572585195
refirst11 <ocelot2828@gmail.com>
2026-08-13 08:46:16 +0900

    chore: export DIRECT_LONGHANDS in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,14 @@ export type { CSSProperties } from './types/css-properties.js';
 export { genBase36Hash } from './utils/hash.js';
 export { transpile } from './utils/transpile.js';
 export { transpileAtomic } from './utils/transpile-atomic.js';
-export { splitAtomicAndNested, processAtomicProps } from './utils/processor-atomic.js';
-export { camelToKebabCase, applyCssValue, isAtRule, exceptionCamelCase } from './utils/helper.js';
+export {
+  splitAtomicAndNested,
+  processAtomicProps,
+} from './utils/processor-atomic.js';
+export {
+  camelToKebabCase,
+  applyCssValue,
+  isAtRule,
+  exceptionCamelCase,
+} from './utils/helper.js';
+export { DIRECT_LONGHANDS } from './utils/shorthand-graph.js';
